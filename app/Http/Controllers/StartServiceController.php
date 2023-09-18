e4af73f95d2d4094ad63812f848b58c8673c8166
aul14 <rahmanaulia336@gmail.com>
2023-09-18 13:46:31 +0700

    update

--- app/Http/Controllers/StartServiceController.php
+++ app/Http/Controllers/StartServiceController.php
@@ -44,162 +44,173 @@ class StartServiceController extends Controller
                         }
                         $do = DoModel::where('no_do', $req->no_do)->first();
 
-                        if ($data->Buzzer == 0 && ($data->statusMesin == 'stop' && $data->mode == 'produksi') && $rtm->status != 'downtime') {
+                        if ($req->status == 'running') {
+                            if ($data->Buzzer == 0 && ($data->statusMesin == 'stop' && $data->mode == 'produksi') && $rtm->status != 'downtime') {
 
-                            DB::rollback();
+                                DB::rollback();
 
-                            $event_arr[] = [
-                                'msg' => 'success',
-                                'code' => 'stop_produksi',
-                                'desc' => "This {$rtm->mesin->name} has not been started, has it been started?",
-                                'oee' => $data,
-                                'target' => $rtm->target,
-                                'nama_mesin' => $rtm->mesin->name,
-                                'operator' => $rtm->operator,
-                                'status' => $rtm->status,
-                                'mesin_id' => $req->mesin_id,
-                                'time' => date("Y-m-d H:i:s", strtotime($rtm->created_at))
-                            ];
-                        } else if ($data->Buzzer == 0 && ($data->statusMesin == 'running' && $data->mode == 'produksi') && $rtm->status != 'downtime') {
-                            $rtm->status = $data->mode;
-                            $rtm->actual = $data->actual == null ? 0 : $data->actual;
-                            $rtm->ng = $data->notGood == 'NaN' ? 0 : $data->notGood;
-                            $rtm->cycle_time = $data->cycleTime;
-                            $rtm->update();
+                                $event_arr[] = [
+                                    'msg' => 'success',
+                                    'code' => 'stop_produksi',
+                                    'desc' => "This {$rtm->mesin->name} has not been started, has it been started?",
+                                    'oee' => $data,
+                                    'target' => $rtm->target,
+                                    'nama_mesin' => $rtm->mesin->name,
+                                    'operator' => $rtm->operator,
+                                    'status' => $rtm->status,
+                                    'mesin_id' => $req->mesin_id,
+                                    'time' => date("Y-m-d H:i:s", strtotime($rtm->created_at))
+                                ];
+                            } else if ($data->Buzzer == 0 && ($data->statusMesin == 'running' && $data->mode == 'produksi') && $rtm->status != 'downtime') {
+                                $rtm->status = $data->mode;
+                                $rtm->actual = $data->actual == null ? 0 : $data->actual;
+                                $rtm->ng = $data->notGood == 'NaN' ? 0 : $data->notGood;
+                                $rtm->cycle_time = $data->cycleTime;
+                                $rtm->update();
 
-                            $do->actual = $data->actual == null ? 0 : $data->actual;
-                            $do->ng = $data->notGood == 'NaN' ? 0 : $data->notGood;
-                            $do->update();
+                                $do->actual = $data->actual == null ? 0 : $data->actual;
+                                $do->ng = $data->notGood == 'NaN' ? 0 : $data->notGood;
+                                $do->update();
 
-                            DB::commit();
+                                DB::commit();
 
-                            $event_arr[] = [
-                                'msg' => 'success',
-                                'code' => 'running',
-                                'desc' => "This {$rtm->mesin->name} has been started and is working on production.",
-                                'oee' => $data,
-                                'target' => $rtm->target,
-                                'nama_mesin' => $rtm->mesin->name,
-                                'operator' => $rtm->operator,
-                                'status' => $rtm->status,
-                                'mesin_id' => $req->mesin_id,
-                                'time' => date("Y-m-d H:i:s", strtotime($rtm->created_at))
-                            ];
-                        } else if ($data->Buzzer == 1 && ($data->statusMesin == 'stop' && $data->mode == 'produksi') && $rtm->status != 'downtime') {
+                                $event_arr[] = [
+                                    'msg' => 'success',
+                                    'code' => 'running',
+                                    'desc' => "This {$rtm->mesin->name} has been started and is working on production.",
+                                    'oee' => $data,
+                                    'target' => $rtm->target,
+                                    'nama_mesin' => $rtm->mesin->name,
+                                    'operator' => $rtm->operator,
+                                    'status' => $rtm->status,
+                                    'mesin_id' => $req->mesin_id,
+                                    'time' => date("Y-m-d H:i:s", strtotime($rtm->created_at))
+                                ];
+                            } else if ($data->Buzzer == 1 && ($data->statusMesin == 'stop' && $data->mode == 'produksi') && $rtm->status != 'downtime') {
 
-                            $lm = new LogMesin();
-                            $date_time = date('Y-m-d H:i:s');
+                                $lm = new LogMesin();
+                                $date_time = date('Y-m-d H:i:s');
 
-                            $lm_update = LogMesin::where('no_do', $req->no_do)->whereNull('log_time_stop')->first();
-                            $start = new DateTime($lm_update->log_time_start);
-                            $end = new DateTime($date_time);
-                            $durasi = $start->diff($end);
-                            $format_durasi = $durasi->format('%H:%i:%s');
+                                $lm_update = LogMesin::where('no_do', $req->no_do)->whereNull('log_time_stop')->first();
+                                $start = new DateTime($lm_update->log_time_start);
+                                $end = new DateTime($date_time);
+                                $durasi = $start->diff($end);
+                                $format_durasi = $durasi->format('%H:%i:%s');
 
-                            $nm = new NotifMesin();
-                            $nm->no_do = $req->no_do;
-                            $nm->mesin_id = $req->mesin_id;
-                            $nm->mesin_name = $rtm->mesin->name;
-                            $nm->do_name = $rtm->do->name;
-                            $nm->status = 'pending';
-                            $nm->time_start = $date_time;
-                            $nm->save();
+                                $nm = new NotifMesin();
+                                $nm->no_do = $req->no_do;
+                                $nm->mesin_id = $req->mesin_id;
+                                $nm->mesin_name = $rtm->mesin->name;
+                                $nm->do_name = $rtm->do->name;
+                                $nm->status = 'pending';
+                                $nm->time_start = $date_time;
+                                $nm->save();
 
-                            $rtm->status = 'downtime';
-                            $rtm->actual = $data->actual == null ? 0 : $data->actual;
-                            $rtm->ng = $data->notGood == 'NaN' ? 0 : $data->notGood;
-                            $rtm->cycle_time = $data->cycleTime;
-                            $rtm->update();
+                                $rtm->status = 'downtime';
+                                $rtm->actual = $data->actual == null ? 0 : $data->actual;
+                                $rtm->ng = $data->notGood == 'NaN' ? 0 : $data->notGood;
+                                $rtm->cycle_time = $data->cycleTime;
+                                $rtm->update();
 
-                            $lm_update->log_time_stop = $date_time;
-                            $lm_update->duration = $format_durasi;
-                            $lm_update->update();
+                                $lm_update->log_time_stop = $date_time;
+                                $lm_update->duration = $format_durasi;
+                                $lm_update->update();
 
-                            $lm->realtime_mesin_id = $rtm->id;
-                            $lm->no_do = $req->no_do;
-                            $lm->log_time_start = $date_time;
-                            $lm->status = 'downtime';
-                            $lm->type = 'downtime';
-                            $lm->created_by = 'admin';
-                            $lm->save();
+                                $lm->realtime_mesin_id = $rtm->id;
+                                $lm->no_do = $req->no_do;
+                                $lm->log_time_start = $date_time;
+                                $lm->status = 'downtime';
+                                $lm->type = 'downtime';
+                                $lm->created_by = 'admin';
+                                $lm->save();
 
-                            DB::commit();
+                                DB::commit();
 
-                            $event_arr[] = [
-                                'msg' => 'success',
-                                'code' => 'stop_perbaikan',
-                                'triger' => 2,
-                                'desc' => "This {$rtm->mesin->name} is off, please check the machine and notify whether it is being repaired or finished.",
-                                'oee' => $data,
-                                'target' => $rtm->target,
-                                'nama_mesin' => $rtm->mesin->name,
-                                'operator' => $rtm->operator,
-                                'status' => $rtm->status,
-                                'mesin_id' => $req->mesin_id,
-                                'time' => date("Y-m-d H:i:s", strtotime($rtm->created_at))
-                            ];
-                        } else if ($data->Buzzer == 1 && ($data->statusMesin == 'stop' && $data->mode == 'produksi') && $rtm->status == 'downtime') {
-                            DB::rollback();
+                                $event_arr[] = [
+                                    'msg' => 'success',
+                                    'code' => 'stop_perbaikan',
+                                    'triger' => 2,
+                                    'desc' => "This {$rtm->mesin->name} is off, please check the machine and notify whether it is being repaired or finished.",
+                                    'oee' => $data,
+                                    'target' => $rtm->target,
+                                    'nama_mesin' => $rtm->mesin->name,
+                                    'operator' => $rtm->operator,
+                                    'status' => $rtm->status,
+                                    'mesin_id' => $req->mesin_id,
+                                    'time' => date("Y-m-d H:i:s", strtotime($rtm->created_at))
+                                ];
+                            } else if ($data->Buzzer == 1 && ($data->statusMesin == 'stop' && $data->mode == 'produksi') && $rtm->status == 'downtime') {
+                                DB::rollback();
 
-                            $event_arr[] = [
-                                'msg' => 'success',
-                                'code' => 'downtime',
-                                'triger' => 1,
-                                'desc' => "This {$rtm->mesin->name} is stopped.",
-                                'oee' => $data,
-                                'target' => $rtm->target,
-                                'nama_mesin' => $rtm->mesin->name,
-                                'operator' => $rtm->operator,
-                                'status' => $rtm->status,
-                                'mesin_id' => $req->mesin_id,
-                                'time' => date("Y-m-d H:i:s", strtotime($rtm->created_at))
-                            ];
-                        } else if ($data->Buzzer == 0 && ($data->statusMesin == 'stop' && $data->mode == 'Finish') && $rtm->status != 'downtime') {
-                            OeeService::where('mesin_id', $req->mesin_id)->where('no_do', $req->no_do)->delete();
+                                $event_arr[] = [
+                                    'msg' => 'success',
+                                    'code' => 'downtime',
+                                    'triger' => 1,
+                                    'desc' => "This {$rtm->mesin->name} is stopped.",
+                                    'oee' => $data,
+                                    'target' => $rtm->target,
+                                    'nama_mesin' => $rtm->mesin->name,
+                                    'operator' => $rtm->operator,
+                                    'status' => $rtm->status,
+                                    'mesin_id' => $req->mesin_id,
+                                    'time' => date("Y-m-d H:i:s", strtotime($rtm->created_at))
+                                ];
+                            } else if ($data->Buzzer == 0 && ($data->statusMesin == 'stop' && $data->mode == 'Finish') && $rtm->status != 'downtime') {
+                                OeeService::where('mesin_id', $req->mesin_id)->where('no_do', $req->no_do)->delete();
 
-                            DB::commit();
+                                DB::commit();
 
-                            $event_arr[] = [
-                                'msg' => 'success',
-                                'code' => 'stop_finish',
-                                'desc' => "This {$rtm->mesin->name} is finished.",
-                                'oee' => $data,
-                                'target' => $rtm->target,
-                                'nama_mesin' => $rtm->mesin->name,
-                                'operator' => $rtm->operator,
-                                'status' => $rtm->status,
-                                'mesin_id' => $req->mesin_id,
-                                'time' => date("Y-m-d H:i:s", strtotime($rtm->created_at))
-                            ];
-                        } else if ($data->Buzzer == 0 && ($data->statusMesin == 'stop' && $data->mode == 'perbaikan') && $rtm->status != 'downtime') {
-                            DB::rollback();
+                                $event_arr[] = [
+                                    'msg' => 'success',
+                                    'code' => 'stop_finish',
+                                    'desc' => "This {$rtm->mesin->name} is finished.",
+                                    'oee' => $data,
+                                    'target' => $rtm->target,
+                                    'nama_mesin' => $rtm->mesin->name,
+                                    'operator' => $rtm->operator,
+                                    'status' => $rtm->status,
+                                    'mesin_id' => $req->mesin_id,
+                                    'time' => date("Y-m-d H:i:s", strtotime($rtm->created_at))
+                                ];
+                            } else if ($data->Buzzer == 0 && ($data->statusMesin == 'stop' && $data->mode == 'perbaikan') && $rtm->status != 'downtime') {
+                                DB::rollback();
 
-                            $event_arr[] = [
-                                'msg' => 'success',
-                                'code' => 'perbaikan',
-                                'desc' => "This {$rtm->mesin->name} is under maintenance.",
-                                'oee' => $data,
-                                'target' => $rtm->target,
-                                'nama_mesin' => $rtm->mesin->name,
-                                'operator' => $rtm->operator,
-                                'status' => $rtm->status,
-                                'mesin_id' => $req->mesin_id,
-                                'time' => date("Y-m-d H:i:s", strtotime($rtm->created_at))
-                            ];
+                                $event_arr[] = [
+                                    'msg' => 'success',
+                                    'code' => 'perbaikan',
+                                    'desc' => "This {$rtm->mesin->name} is under maintenance.",
+                                    'oee' => $data,
+                                    'target' => $rtm->target,
+                                    'nama_mesin' => $rtm->mesin->name,
+                                    'operator' => $rtm->operator,
+                                    'status' => $rtm->status,
+                                    'mesin_id' => $req->mesin_id,
+                                    'time' => date("Y-m-d H:i:s", strtotime($rtm->created_at))
+                                ];
+                            } else {
+                                DB::rollback();
+
+                                $event_arr[] = [
+                                    'msg' => 'error',
+                                    'desc' => 'System error please contact the Support team',
+                                    'mesin_id' => $req->mesin_id
+                                ];
+
+                                Log::info([
+                                    'msg' => 'error',
+                                    'desc' => 'System error please contact the Support team',
+                                    'mesin_id' => $req->mesin_id
+                                ]);
+                            }
                         } else {
                             DB::rollback();
 
                             $event_arr[] = [
-                                'msg' => 'error',
-                                'desc' => 'System error please contact the Support team',
-                                'mesin_id' => $req->mesin_id
+                                'msg' => 'success',
+                                'code' => 'mesin_perbaikan',
+                                'desc' => "This {$rtm->mesin->name} is under maintenance.",
+                                'time' => date("Y-m-d H:i:s", strtotime($rtm->created_at))
                             ];
-
-                            Log::info([
-                                'msg' => 'error',
-                                'desc' => 'System error please contact the Support team',
-                                'mesin_id' => $req->mesin_id
-                            ]);
                         }
                     } catch (QueryException $th) {
                         DB::rollback();
